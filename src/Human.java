9c0077e01ba9e65b1d2d9b0ff006ce1aaf095126
Daniel <deaconescu.daniel11@gmail.com>
2023-05-20 20:36:23 +0300

    Added game loop

--- src/Human.java
+++ src/Human.java
@@ -1,25 +1,25 @@
 import java.util.Random;
-
-public abstract class Human implements Movement {
-
+public  abstract class Human  implements Movement{
     String name;
-    private int age;
-    boolean gender;
-    private String status;
-    byte HP;
+    int age;
+    boolean  gender;
+    //  private  String status;
+    byte HP=100;
+
     byte x;
     byte y;
+    int Oxygen=100;
 
-    @Override
-    public void moveUp(Human h1) {
-        if(h1.x != 0) {
-            h1.x--;
+
+    public void moveUp() {
+        if(x != 0) {
+            x--;
         } else throw new IllegalArgumentException("Movement cannot be posible");
     }
 
     @Override
     public void moveDown(Human h1) {
-        if(h1.x != 9) {
+        if(h1.x != 9 ) {
             h1.x++;
         } else throw new IllegalArgumentException("Movement cannot be posible");
     }
@@ -41,10 +41,20 @@ public abstract class Human implements Movement {
     static final String[] names = {"John", "Michael", "Bill", "Liam", "Emma", "Noah", "Oliver", "Peter", "Chris",
             "Charlotte", "Ethan", "Elizabeth", "Sofia", "Lucas", "James", "Grace", "Alexander", "Chloe", "Daniel",
             "Roxane", "Robert", "David", "Samuel", "Lilly", "Joseph", "Henry", "Ella", "Penelope", "Amelia", "Evelyn"};
+    ;
 
+    static final int[] ages={24,30,18,19,21,32,25,20,22,23};
+    public static int generateAge()
+    {
+        Random random=new Random();
+        int randomAge=ages[random.nextInt(ages.length)];
+        return randomAge;
+    }
     public static String generateName() {
         Random random = new Random();
         String randomName = names[random.nextInt(names.length)];
         return randomName;
     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,73 @@ import java.util.Scanner;
 public class Main {
     static Tile[][] Mars = new Tile[10][10];
 
-    public static void main(String[] args) {
+    public static void printTable() {
+        for (byte i = 0; i < 10; i++) {
+            for (byte j = 0; j < 10; j++) {
+                if (Mars[i][j].presents.size() == 0) {
+                    System.out.print("[    ]" + " ");
+                }
+                for (Human object : Mars[i][j].presents) {
+                    System.out.print("[ " + object.name + " ]" + " ");
+                }
 
-        System.out.println("Welcome to Mars Colonazation! Please type START to start the game!");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printteam(ArrayList<? extends Human> inarr) {
+        for (Human colonist : inarr) {
+            if (colonist.HP != 0) {
+                System.out.println(colonist.name + ", " + colonist.age + " y/o " + colonist.HP + "/100 HP");
+            } else {
+                System.out.println(colonist.name + "DEAD");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int turnnumber = 1;
+        System.out.println("Welcome to Mars Colonization!Please type START to start the game, or type HELP for a list of commands and tips.");
         Scanner scan = new Scanner(System.in);
         String variabileStart = scan.next();
         variabileStart = variabileStart.toUpperCase();
-        while(!variabileStart.equals("START")) {
+        if(variabileStart.equals("HELP")){
+            System.out.println("Your objective is to explore Mars.");
+            System.out.println("Every turn, you can see your team and their status at the top.");
+            System.out.println("You win if you explore the whole of Mars. You lose if your whole team dies.");
+            System.out.println("Select a team member using the \"select\" command.");
+            System.out.println("Once a team member is selected, use \"moveup\", \"movedown\",\"moveleft\" or \"moveright\" to move, Astronauts cannot move more than once in a turn.");
+            System.out.println("Unexplored tiles are marked with ?, explore them by moving an Astronaut over them. Colonists are the best for exploring, as other roles have higher chances of being injured");
+            System.out.println("Medics may heal other Astornauts using the \"heal\" command, Engineers may repair other Astronauts' equipment using the \"repair\" command");
+            System.out.println("Once you are ready to proceed, use the \"NextTurn\" command to move on to the next turn");
+        }
+        while (!variabileStart.equals("START")) {
             System.out.println("Please type START to start the game");
             variabileStart = scan.next();
             variabileStart = variabileStart.toUpperCase();
         }
+
         System.out.println("Please choose how many engineers do you want: ");
         Scanner scan1 = new Scanner(System.in);
         int numberOfEngineers = scan.nextInt();
-        while(numberOfEngineers <= 0) {
+        while (numberOfEngineers <= 0) {
             System.out.println("Please introduce a number of engineers greater than 0");
             numberOfEngineers = scan.nextInt();
         }
+
         System.out.println("Please choose how many medics do you want: ");
         Scanner scan2 = new Scanner(System.in);
         int numberOfMedics = scan.nextInt();
-        while(numberOfMedics <= 0) {
+        while (numberOfMedics <= 0) {
             System.out.println("Please introduce a number of medics greater than 0");
             numberOfMedics = scan.nextInt();
         }
+
         System.out.println("Please choose how many colonists do you want: ");
         Scanner scan3 = new Scanner(System.in);
         int numberOfColonists = scan.nextInt();
-        while(numberOfColonists <= 0) {
+        while (numberOfColonists <= 0) {
             System.out.println("Please introduce a number of colonist greater than 0");
             numberOfColonists = scan.nextInt();
         }
@@ -47,46 +85,82 @@ public class Main {
         ArrayList<Medic> medics = new ArrayList<>(numberOfMedics);
         ArrayList<Colonist> colonists = new ArrayList<>(numberOfColonists);
 
-        for(int i = 0; i < numberOfEngineers; i++) {
-            Engineer e1 = new Engineer((byte) 4, (byte) 4, Engineer.generateName());
-            System.out.println(e1.name);
+        for (int i = 0; i < numberOfEngineers; i++) {
+            Engineer e1 = new Engineer((byte) 1, (byte) 2, Engineer.generateName(), Engineer.generateAge());
+            System.out.println(e1.name + ", " + e1.age);
             engineers.add(e1);
             Mars[4][4].presents.add(e1);
         }
 
-        for(int i = 0; i < numberOfMedics; i++) {
-            Medic m1 = new Medic((byte) 4, (byte) 4, Medic.generateName());
-            System.out.println(m1.name);
+
+        for (int i = 0; i < numberOfMedics; i++) {
+            Medic m1 = new Medic((byte) 4, (byte) 4, Medic.generateName(), Medic.generateAge());
+            System.out.println(m1.name + ", " + m1.age);
             medics.add(m1);
             Mars[4][4].presents.add(m1);
         }
 
-        for(int i = 0; i < numberOfColonists; i++) {
-            Colonist c1 = new Colonist((byte) 4, (byte) 4, Colonist.generateName());
-            System.out.println(c1.name);
+        for (int i = 0; i < numberOfColonists; i++) {
+            Colonist c1 = new Colonist((byte) 4, (byte) 4, Colonist.generateName(), Colonist.generateAge());
+            System.out.println(c1.name + ", " + c1.age);
             colonists.add(c1);
             Mars[4][4].presents.add(c1);
         }
 
+        //engineers.get(1).moveUp(engineers.get(1));
+        //engineers.get(1).moveUpPlayer();
+        // System.out.println(engineers.get(1).x);
+        //System.out.println(engineers.get(1).y);
 
-//        engineers.get(1).moveUp(engineers.get(1));
-//        System.out.println(engineers.get(1).x);
-//        System.out.println(engineers.get(1).y);
-//
-//        engineers.get(1).moveUp(engineers.get(1));
-//        System.out.println(engineers.get(1).x);
-//        System.out.println(engineers.get(1).y);
-//
-//        engineers.get(1).moveUp(engineers.get(1));
-//        System.out.println(engineers.get(1).x);
-//        System.out.println(engineers.get(1).y);
+        /// colonists.get(1).moveUpPlayer();
 
-        for (byte i = 0; i < 10; i++) {
-            for (byte j = 0; j < 10; j++) {
-                System.out.print(Mars[i][j].presents);
+        printTable();
+        Medic m1 = new Medic((byte) 2, (byte) 2, Medic.generateName(), Medic.generateAge());
+        Colonist c1 = new Colonist((byte) 2, (byte) 3, Colonist.generateName(), Colonist.generateAge());
+        Engineer e1 = new Engineer((byte) 1, (byte) 3, Engineer.generateName(), Engineer.generateAge());
+        //c1.moveUpPlayer();
+        //m1.moveLeftPlayer();;
+        // c1.moveUp();
+        //Damage d1=new Damage();
+        //d1.moveUp();
+
+        //System.out.println(engineers.get(0).name);
+        System.out.println("It's time to explore!");
+        while (true) {
+            while (true) {
+                System.out.println("TURN " + turnnumber);
+                System.out.println("Your team:");
+                System.out.println("Medics:");
+                printteam(medics);
+                System.out.println("Engineers:");
+                printteam(engineers);
+                System.out.println("Colonists:");
+                printteam(colonists);
+                String commandText = scan.next();
+                switch (commandText.toLowerCase()) {
+                    case "nextturn":
+                        break;
+                    case "moveup":
+                        break;
+                    case "movedown":
+                        break;
+                    case "moveleft":
+                        break;
+                    case "moveright":
+                        break;
+                    case "heal":
+                        break;
+                    case "repair":
+                        break;
+                    default:
+                        System.out.println("Invalid command. Please try again.");
+                        break;
+                }
+                turnnumber++;
             }
-            System.out.println();
+
         }
     }
+
 
 }
